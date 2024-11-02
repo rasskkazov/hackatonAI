612a08ae38de297cec5b2a3f5ed54c159eea1bfa
bogdan <brasskazov@gmail.com>
2024-11-02 16:15:24 +0300

    feat: msg list ui

--- src/widgets/ChatList/ChatList.tsx
+++ src/widgets/ChatList/ChatList.tsx
@@ -1,10 +1,27 @@
 import * as classes from "./Chatlist.module.scss";
 
 export const ChatList = () => {
+  const messages = [
+    {
+      text: "hello boys",
+      sender: "user",
+    },
+    {
+      text: "hello gays",
+      sender: "bot",
+    },
+    {
+      text: "hello asdfasdf",
+      sender: "user",
+    },
+  ];
   return (
     <div className={classes.chat}>
-      <div className="message">Hello</div>
-      <div className="mesage">Hello</div>
+      <ul className={classes.messageList}>
+        {messages.map((msg, index) => {
+          return <div key={index}>{msg.text}</div>;
+        })}
+      </ul>
     </div>
   );
 };
