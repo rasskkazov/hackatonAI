7402d1e6589410635dc3cd1247a1b43be113b325
bogdan <brasskazov@gmail.com>
2024-11-03 17:24:35 +0300

    fix: remove i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,15 @@
 import { Suspense } from "react";
-import { useTranslation } from "react-i18next";
 import { RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./router/Router";
 
 import { queryClient } from "@/shared/api/queryClient";
 
-import "@/shared/locales/i18n";
 import "./App.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 
 export const App = () => {
-  const { t, i18n } = useTranslation();
-
   return (
     <Suspense fallback="...Loading">
       <QueryClientProvider client={queryClient}>
--- src/shared/api/apiToken.ts
+++
@@ -1,1 +0,0 @@
-export const API_TOKEN = process.env.TOKEN ?? process.env.TOKEN_DEFAULT;
